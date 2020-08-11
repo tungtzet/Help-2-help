65ffbdd5e8b6fd0b5f89e8492034fdd843c5ce95
nicolo torromacco <nicolotor@gmail.com>
2020-08-11 12:27:27 +0200

    added users and profiles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+languages = ['German', 'French', 'Italian' , 'Vietnamese']
+languages_second = ['Chinese', 'Russian', 'Dutch' , 'Greek']
+# Faker::Name.name
+User.destroy_all
+Profile.destroy_all
+30.times do
+  user = User.create!(email: Faker::Internet.email, password: '123456')
+  Profile.create!(user_id: user.id, address: "#{Faker::Address.city}, #{Faker::Address.country}", age: rand(18..99), native_language: languages.sample, second_language: languages_second.sample, bio: Faker::Marketing.buzzwords, name: Faker::Name.name)
+end
+
