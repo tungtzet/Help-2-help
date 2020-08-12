f3a37c3f7ca789e793555fd8f06647f516bb8096
Andreas Wittenberg <projekt@webdesignagentur.design>
2020-08-12 15:05:07 +0200

    works with post foto attatched

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,6 +34,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, :user_id)
+    params.require(:post).permit(:title, :content, :user_id, photos: [])
   end
 end
