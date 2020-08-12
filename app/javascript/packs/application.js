076f049e065b33bb7698b17f08d5775d40210f7f
tung nguyen <tungnt@arena.edu.vn>
2020-08-12 20:52:17 +0200

    fix style

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,7 +26,7 @@ require("channels")
 import "bootstrap";
 // Internal imports, e.g:
 import { initSelect2 } from '../plugins/init_select2';
-import { initAutocomplete } from '../plugins/init_autocomplete'
+import { initAutocomplete } from '../plugins/init_autocomplete';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
