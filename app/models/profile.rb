a4fffb32087457c800d41ba0e71e2162f8424e8f
tung nguyen <tungnt@arena.edu.vn>
2020-08-18 10:38:27 +0200

    include condition in the form

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -6,6 +6,7 @@ class Profile < ApplicationRecord
   has_one_attached :photo
 
   validates :name, presence: true
+  validates :disease, presence: true
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
